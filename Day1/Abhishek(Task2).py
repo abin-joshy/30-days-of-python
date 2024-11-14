d72024b0db9a54a65449cb1c3119987435c02e43
AbhishekDNair23 <abhishekdnair2028@cs.sjcetpalai.ac.in>
2024-11-14 23:29:19 +0530

    Add files via upload

---
+++ Day1/Abhishek(Task2).py
@@ -0,0 +1,8 @@
+'''
+Author : Abhishek
+Date : 13-11-2024
+Description : The input() function is used to take input from the user.
+It reads a line from the user’s input and returns it as a string.
+'''
+name = input("Enter your name:")
+print("Your name is",name)
