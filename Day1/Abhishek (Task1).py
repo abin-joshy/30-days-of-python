e8451295b5443c692769bb3b4c79a9c96f3582d8
AbhishekDNair23 <abhishekdnair2028@cs.sjcetpalai.ac.in>
2024-11-14 23:23:51 +0530

    Add files via upload

---
+++ Day1/Abhishek (Task1).py
@@ -0,0 +1,9 @@
+'''
+Author : Abhishek
+Date : 13-11-2024
+Description : The print() function prints the specified message to the screen or other standard output devices.
+ The message can be a string or any other object; the object will be converted into a string before being displayed.
+'''
+print("Hello, World!")
+print("Welcome to the Python Challenge!")
+print("Let's make learning Python fun!")
