07dff13b14b4b0a78863955a13fe7e68a630f77b
abi-2006 <abishastjoseph27@gmail.com>
2024-11-16 21:36:46 +0530

    Add files via upload

---
+++ Day2/abiday2task1.py
@@ -0,0 +1,17 @@
+num1=int(input("enter the first  number:"))
+num2=int(input("enter the second number:"))
+sum=num1 + num2
+print("sum of num1 and num2 is:",sum)
+difference=num1 - num2
+print("difference of num1 and num2 is:",difference)
+product=num1*num2
+print("product of num1 and num2 is:",product)
+quotient=num1/num2
+print("the quotient of num1 and num2 is:",quotient)
+floor_division=num1//num2
+print("the answer of floor division of num1 and num2 is:",floor_division)
+power=num1**num2
+print("the power of num1 and num2 is:",power)
+modulus=num1%num2
+print("the modulus of num1 and num2 is:",modulus)
+
