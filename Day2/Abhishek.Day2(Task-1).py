ca144004990e289f5ad572d233b6db403545de41
AbhishekDNair23 <abhishekdnair2028@cs.sjcetpalai.ac.in>
2024-11-17 20:54:29 +0530

    Add files via upload

---
+++ Day2/Abhishek.Day2(Task-1).py
@@ -0,0 +1,16 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+Sum = a+b
+print(f"Sum is:{Sum}")
+Difference = a-b
+print(f"Difference is:{Difference}")
+Product = a*b
+print(f"Product is:{Product}")
+Quotient = a/b
+print(f"Quotient is:{Quotient}")
+Floor= a//b
+print(f"Floor is:{Floor}")
+Power = a**b
+print(f"Power is:{Power}")
+Modulus = a%b
+print(f"Modulus is:{Modulus}")
