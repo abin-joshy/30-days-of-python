db354077db27798942239967c7c8a377160f2dfb
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2024-11-17 08:56:09 +0530

    Add files via upload

---
+++ Day3/ageena3task1.py
@@ -0,0 +1,16 @@
+import math
+user=int(input("Enter the number:"))
+user2=int(input("Enter the 2nd number:"))
+print(math.sqrt(user))
+print(math.floor(user))
+print(math.pow(user,user2))
+print(math.factorial(user))
+if user==0:
+    print("The number does not have a log value")
+else:
+    print(math.log(user))
+print(math.pi)
+print(math.e)
+print(math.sin(user))
+print(math.cos(user))
+print(math.tan(user))
