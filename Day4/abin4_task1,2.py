b71116387e0055491bd54b6cd0e78db30271a0bd
abin-joshy <abinkuriakosejoshy2028@cs.sjcetpalai.ac.in>
2024-11-21 21:42:34 +0500

    Add files via upload

---
+++ Day4/abin4_task1,2.py
@@ -0,0 +1,9 @@
+#task1
+word=input("enter a word:")
+letter=input("enter a letter:")
+print("Is the letter is in the word:",letter in word)
+
+#task2
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+print("Are both numbers the same object in memory:",num1 is num2)
