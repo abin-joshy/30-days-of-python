1eb9a9b257556efc7ce41342397353ba1dcdc0f9
abin-joshy <abinkuriakosejoshy2028@cs.sjcetpalai.ac.in>
2024-11-21 21:28:18 +0500

    Add files via upload

---
+++ Day3/abin3_task1.py
@@ -0,0 +1,21 @@
+import math
+
+num=int(input("enter a number:"))
+
+#common functions in math module
+print(math.sqrt(num))
+print(math.floor(num))
+print(math.pow(num,2))
+print(math.factorial(num))
+print(math.log(num))
+
+#constants in math module
+print(math.pi)
+print(math.e)
+
+#trignometric functions in math module
+print(math.sin(num))
+print(math.cos(num))
+print(math.tan(num))
+print(math.acos(num))
+print(math.atan(num))
