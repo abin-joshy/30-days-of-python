f53286a14afd3053cd84a2383b2b41fb3c754a54
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2024-11-23 22:44:29 +0530

    Add files via upload

---
+++ Day5/ageena5.py
@@ -0,0 +1,14 @@
+str='Hello World!'
+str_double="Hello World!"
+str_triple='''Hello World!'''
+print(str)
+print(str_double)
+print(str_triple)
+print(len(str))
+print(str.lower())
+print(str.upper())
+print(str.title())
+print(str.swapcase())
+print(str.capitalize())
+print(str.replace('World','Guyss'))
+print(str.split("o"))
